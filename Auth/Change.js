e2ae5b87a77dac0e38cd45f464f2843cd42b94db
Lucas Sifoni <lucas@lucassifoni.info>
2017-02-28 22:36:59 +0100

    Everything auth-related

---
+++ Auth/Change.js
@@ -0,0 +1,17 @@
+/**
+ * Created by lucas on 20/02/17.
+ */
+/* eslint-disable no-trailing-spaces, quotes*/
+
+export default {
+  fr: {
+    title: `Changer de mot de passe`,
+    newPass: 'Nouveau mot de passe',
+    confirm: 'Confirmer le nouveau mot de passe',
+  },
+  en: {
+    title: `Change my password`,
+    newPass: 'New password',
+    confirm: 'Confirm the new password',
+  },
+};
---
+++ Auth/Delete.js
@@ -0,0 +1,17 @@
+/**
+ * Created by lucas on 20/02/17.
+ */
+/* eslint-disable no-trailing-spaces, quotes*/
+
+export default {
+  fr: {
+    title: `Supprimer mon compte`,
+    intro: `Cette action est irréversible. Vos publications seront anonymisées, et vous recevrez un e-mail confirmant la suppression de votre compte. Vous devez taper votre mot de passe pour confirmer la suppression.`,
+    pass: `mot de passe`,
+  },
+  en: {
+    title: `Delete my account`,
+    intro: `This action is irreversible. Your published content will become anonymous, and you'll get an e-mail that confirms your account's deletion. You have to type your password to confirm your account deletion`,
+    pass: `password`,
+  },
+};
