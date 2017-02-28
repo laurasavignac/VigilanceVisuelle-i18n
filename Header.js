049ee54a332581b1570fbc7b1c545fae9327867f
Lucas Sifoni <lucas@lucassifoni.info>
2017-02-28 22:36:23 +0100

    Added Header

---
+++ Header.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-trailing-spaces, quotes*/
+
+export default {
+  fr: {
+    title: 'Vigilance visuelle',
+    description: `Observatoire contributif de veille des contenus visuels dans l'espace public.`,
+  },
+  en: {
+    title: 'Visual vigilance',
+    description: `Contributive observatory to encourage awareness of visual content in the public space.`,
+  },
+};
